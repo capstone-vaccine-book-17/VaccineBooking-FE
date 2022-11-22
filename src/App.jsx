b7508abd1e1220e92d0108c3cd6debfdca0e1195
moonix0 <moongraph21@gmail.com>
2022-11-22 11:56:05 +0700

    Initialize Project Using CRA

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center gap-2">
+      <h1 className="text-3xl font-semibold text-slate-700">
+        Vaccine Booking - FrontEnd
+      </h1>
+      <p className="text-xl font-light text-slate-500">React + TailwindCSS</p>
+    </div>
+  );
+};
+
+export default App;

b525a77fc2d12c31febeea74a84dae77384e650f
moonix0 <moongraph21@gmail.com>
2022-11-25 19:43:25 +0700

    Fixing All Dashboards

--- src/pages/dashboard/ManageBooking.jsx
+++ src/pages/dashboard/ManageBooking.jsx
@@ -1,13 +1,80 @@
 import React from "react";
+import RecapDashboard from "../../components/RecapDashboard";
+import ButtonAdd from "../../components/ButtonAdd";
 
 const ManageBooking = () => {
   const datas = [
-    {no: "1", nama:"Tengku", nik:"1571021703010022", waktu:"08:00 - 11:00", tanggal:"2022-11-14", dosis:"Pertama", antrian:"1", status:"Proses"},
-    {no: "2", nama:"Afifah", nik:"1571021703010022", waktu:"08:00 - 11:00", tanggal:"2022-11-14", dosis:"Pertama", antrian:"2", status:"Proses"},
-    {no: "3", nama:"Eridani", nik:"1571021703010022", waktu:"08:00 - 11:00", tanggal:"2022-11-14", dosis:"Pertama", antrian:"3", status:"Proses"},
-  ]
+    {
+      no: "1",
+      nama: "Tengku",
+      nik: "1571021703010022",
+      waktu: "08:00 - 11:00",
+      tanggal: "2022-11-14",
+      dosis: "Pertama",
+      antrian: "1",
+      status: "Proses",
+    },
+    {
+      no: "2",
+      nama: "Afifah",
+      nik: "1571021703010022",
+      waktu: "08:00 - 11:00",
+      tanggal: "2022-11-14",
+      dosis: "Pertama",
+      antrian: "2",
+      status: "Proses",
+    },
+    {
+      no: "3",
+      nama: "Eridani",
+      nik: "1571021703010022",
+      waktu: "08:00 - 11:00",
+      tanggal: "2022-11-14",
+      dosis: "Pertama",
+      antrian: "3",
+      status: "Proses",
+    },
+  ];
+
   return (
     <section className="h-full w-full px-8 mt-8">
+      <RecapDashboard title="Manage Booking" />
+      <div className="flex items-center my-8 gap-48">
+        <div className="flex-1">
+          <form className="flex gap-8">
+            <div className="relative w-full">
+              <input
+                className="flex-[70%] bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-2.5"
+                type="text"
+                placeholder="Cari......"
+              />
+              <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+            <select className="flex-[30%] bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-300 focus:border-gray-300 block w-full p-2.5">
+              <option value="" disabled selected>
+                Urut Berdasarkan
+              </option>
+              <option value="">Tanggal</option>
+              <option value="">Waktu</option>
+            </select>
+          </form>
+        </div>
+        <ButtonAdd pathFor="add-sesi" btnFor="Tambah Booking" />
+      </div>
       <table className="w-full rounded-lg shadow-md text-[#373737] bg-white">
         <thead>
           <tr className="border-b-[1px]">
@@ -34,15 +101,18 @@ const ManageBooking = () => {
               <th className="font-normal">{data.antrian}</th>
               <th className="font-normal">{data.status}</th>
               <th className="w-[240px] flex justify-center items-center gap-4 py-4 px-6 font-normal">
-                <img src="" alt="del" />
-                <img src="" alt="edit" />
+                <button className="bg-red-500 hover:bg-gray-700 text-white py-2 px-4 mr-3 rounded">
+                  <img src="" alt="del" />
+                </button>
+                <button className="bg-[#0057FF] hover:bg-gray-700 text-white py-2 px-4 rounded">
+                  <img src="" alt="edit" />
+                </button>
               </th>
             </tr>
           ))}
         </tbody>
       </table>
     </section>
-    
   );
 };
 

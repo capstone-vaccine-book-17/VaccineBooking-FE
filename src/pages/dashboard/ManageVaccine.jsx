377211b16e624e4ae66006217b6a78858e9ed76a
moonix0 <moongraph21@gmail.com>
2022-11-25 16:11:20 +0700

    Feat/Vaksin

--- src/pages/dashboard/ManageVaccine.jsx
+++ src/pages/dashboard/ManageVaccine.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import RecapCard from "../../components/RecapCard";
+import RecapDashboard from "../../components/RecapDashboard";
 
 const ManageVaccine = () => {
+  const datas = [
+    {
+      no: 1,
+      jenis: "Corona Vac",
+      kuota: "2000",
+      expired: "2023-11-14",
+    },
+    {
+      no: 2,
+      jenis: "Moderna",
+      kuota: "1000",
+      expired: "2023-11-14",
+    },
+    {
+      no: 3,
+      jenis: "AstraZeneca",
+      kuota: "3000",
+      expired: "2023-11-14",
+    },
+  ];
+
   return (
-    <section className="h-full flex justify-center items-center text-3xl">
-      Manage Vaccine
+    <section className="h-full w-full px-8 mt-8">
+      <RecapDashboard title="Manage Vaksin" />
+      <table className="w-full rounded-lg shadow-md text-[#373737] bg-white">
+        <thead>
+          <tr className="border-b-[1px]">
+            <th className="py-4">No</th>
+            <th className="py-4">Jenis Vaksin</th>
+            <th className="py-4">Kuota</th>
+            <th className="py-4">Expired Time</th>
+            <th className="w-[240px] py-4">Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          {datas.map((data) => (
+            <tr key={data.no} className="border-b-[1px]">
+              <th className="font-normal">{data.no}</th>
+              <th className="font-normal">{data.jenis}</th>
+              <th className="font-normal">{data.kuota}</th>
+              <th className="font-normal">{data.expired}</th>
+              <th className="w-[240px] flex justify-center items-center gap-4 py-4 px-6 font-normal">
+                <img src="" alt="del" />
+                <img src="" alt="edit" />
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </section>
   );
 };

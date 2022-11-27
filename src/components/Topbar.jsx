7d8a988ad1dd064e70b4ac79c1909943c522e605
moonix0 <moongraph21@gmail.com>
2022-11-27 20:33:06 +0700

    Add some icons

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
+import Notify from "../assets/notify.png";
 
 const Topbar = () => {
   return (
-    <div className='w-full'>
-      <nav className='flex justify-between items-center px-8 py-8 bg-[#ffffff]'>
+    <div className="w-full">
+      <nav className="flex justify-between items-center px-8 py-8 bg-[#ffffff]">
         <div>
-          <h1 className='text-2xl'>Welcome</h1>
+          <h1 className="text-2xl">Welcome</h1>
         </div>
-        <div className='flex gap-12'>
-          <img src='' alt='icon' />
-          <img src='' alt='profile' />
+        <div className="flex gap-12">
+          <img src={Notify} alt="icon" />
+          <img src="" alt="profile" />
         </div>
       </nav>
     </div>

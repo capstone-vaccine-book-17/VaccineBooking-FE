0c3c8f51469b7b682ee9a5656a65a0cdfd3f7e65
Halimah <halimahtjoa@gmail.com>
2022-12-01 21:12:50 +0700

    LandingPage/About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import vaccinePana from "../assets/vaccinePana.svg";
+import polygon from "../assets/polygon.svg";
 
 const About = () => {
   return (
-    <section className="w-full flex justify-center text-6xl">
-      <div>About</div>
+    <section className="w-screen">
+      <div className="w-full">
+        <div className="flex justify-center text-4xl font-semibold mt-10">
+          <h1>Tentang World Vaccine</h1>
+        </div>
+        <div className="flex justify-end mr-[105px] mb-7">
+          <img src={polygon} alt="" />
+        </div>
+        <div className="flex justify-around items-center flex-row mb-[50px]">
+          <div className="mr-[66px] ml-[76px]">
+            <img src={vaccinePana} alt="" />
+          </div>
+          <div>
+            <h1 className="text-xl text-[#373737] font-semibold mb-4">
+              Platform layanan kesehatan Mobile
+            </h1>
+            <p className="text-[#888888] text-base font-semibold w-[734px] mr-[64px]">
+              <span className="text-[#3C9F19]">World Vaccine </span> merupakan
+              suatu platform aplikasi mobile yang menyediakan layanan booking
+              vaksin berdasarkan lokasi kamu saat ini dengan harapan dapat
+              membantu kamu dimasa pandemi saat ini.
+              <p className="text-[#050505]">Sayangi Keluargamu, Ayo Vaksin !</p>
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-start ">
+          <hr class="ml-[44px] mt-[50px] mb-[18px] w-10 h-10 bg-[#0015D1] rounded-full border-0" />
+        </div>
+      </div>
     </section>
   );
 };

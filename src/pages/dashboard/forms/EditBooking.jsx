0d830ec77c199e648deee99f5da3aa2d28179d26
moonix0 <moongraph21@gmail.com>
2022-12-08 21:18:24 +0700

    feat/apis: Get Values Status

--- src/pages/dashboard/forms/EditBooking.jsx
+++ src/pages/dashboard/forms/EditBooking.jsx
@@ -1,88 +1,104 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import TopSection from '../../../components/TopSection';
 
 const EditBooking = () => {
+  const { getValues, register, handleSubmit } = useForm();
+
+  const handleEditBooking = () => {
+    console.log(getValues());
+  };
+
   return (
     <section className='h-full w-full px-8 mt-8'>
       <TopSection title='Konfirmasi Booking' />
       <div className='w-full bg-white rounded-md p-8 shadow-md'>
-        <form className='flex justify-between gap-60' action=''>
+        <form
+          onChange={() => getValues()}
+          onSubmit={handleSubmit(handleEditBooking)}
+          className='flex justify-between gap-60'
+        >
           <div className='flex-1 space-y-8'>
             <div className='space-y-2'>
-              <label className='text-xl' for='namaPemesan'>
+              <label className='text-xl' htmlFor='name'>
                 Nama Pemesan
               </label>
               <input
                 className='bg-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-3'
                 type='text'
-                name=''
-                placeholder='Masukkan Nama Pemesan'
+                name='name'
+                disabled
               />
             </div>
             <div className='space-y-2'>
-              <label className='text-xl' for='nik'>
+              <label className='text-xl' htmlFor='nik'>
                 NIK
               </label>
               <input
                 className='bg-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-3'
                 type='text'
-                name=''
-                placeholder='Masukkan NIK Pemesan'
+                name='nik'
+                disabled
               />
             </div>
             <div className='space-y-2'>
-              <label className='text-xl' for='nik'>
+              <label className='text-xl' htmlFor='time'>
                 Waktu
               </label>
               <input
                 className='bg-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-3'
                 type='time'
-                name=''
-                placeholder='08.00-10.00'
+                name='time'
+                disabled
               />
             </div>
             <div className='space-y-2'>
-              <label className='text-xl' for='nik'>
+              <label className='text-xl' htmlFor='date'>
                 Tanggal
               </label>
               <input
                 className='bg-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-3'
                 type='date'
-                name=''
-                placeholder='2022-10-17'
+                name='date'
+                disabled
               />
             </div>
           </div>
           <div className='flex-1 space-y-8'>
             <div className='space-y-2'>
-              <label className='text-xl' for='Sesi Tersedia'>
+              <label className='text-xl' htmlFor='Sesi Tersedia'>
                 Dosis
               </label>
-              <select className='bg-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-gray-300 focus:border-gray-300 block w-full p-3'>
-                <option value='' disabled selected>
-                  Pilih Dosis
-                </option>
+              <select
+                disabled
+                className='bg-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-gray-300 focus:border-gray-300 block w-full p-3'
+              >
                 <option value='sesi1'>Dosis 1</option>
                 <option value='sesi2'>Dosis 2</option>
                 <option value='sesi3'>Booster</option>
               </select>
             </div>
             <div className='space-y-2'>
-              <label className='text-xl' for='nik'>
+              <label className='text-xl' htmlFor='queue'>
                 Antrian
               </label>
               <input
                 className='bg-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-3'
                 type='number'
-                name=''
-                placeholder='1'
+                name='queue'
+                disabled
               />
             </div>
             <div className='space-y-2'>
-              <label className='text-xl' for='Sesi Tersedia'>
+              <label className='text-xl' htmlFor='status'>
                 Status Booking
               </label>
-              <select className='bg-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-gray-300 focus:border-gray-300 block w-full p-3'>
+              <select
+                {...register('status', {
+                  required: 'Status is required!',
+                })}
+                className='bg-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-gray-300 focus:border-gray-300 block w-full p-3'
+              >
                 <option value='' disabled selected>
                   Pilih Status
                 </option>
@@ -91,16 +107,16 @@ const EditBooking = () => {
                 <option value='batal'>Batal</option>
               </select>
             </div>
+            <div className='w-full flex justify-end '>
+              <button
+                className='mt-16 text-white bg-[#0057FF] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-2xl px-5 py-2.5'
+                type='submit'
+              >
+                Edit Data
+              </button>
+            </div>
           </div>
         </form>
-        <div className='w-full flex justify-end '>
-          <button
-            className='mt-16 text-white bg-[#0057FF] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-2xl px-5 py-2.5'
-            type='submit'
-          >
-            Edit Data
-          </button>
-        </div>
       </div>
     </section>
   );

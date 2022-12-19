74dd6289a2693639749c9437805c688f950e01cb
moonix0 <moongraph21@gmail.com>
2022-12-19 14:21:00 +0700

    feat/apis: Change Logout Position

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { authAdmin } from '../utils/authAdmin';
 import Notify from '../assets/notify.png';
 
 const Topbar = () => {
-  const navigate = useNavigate();
-
   return (
     <div className='w-full'>
       <nav className='flex justify-between items-center px-12 py-5 bg-[#ffffff]'>
@@ -15,14 +11,6 @@ const Topbar = () => {
         <div className='flex items-center gap-12'>
           <img className='w-8 h-8' src={Notify} alt='icon' />
           <div className='w-14 h-14 rounded-full bg-blue-700'></div>
-          <button
-            onClick={() => {
-              authAdmin.setLogout(navigate);
-            }}
-            className='text-[#DB202D] bg-white border-2 border-[#DB202D] px-4 py-2 rounded-[8px] hover:bg-[#DB202D] hover:text-white'
-          >
-            Logout
-          </button>
         </div>
       </nav>
     </div>

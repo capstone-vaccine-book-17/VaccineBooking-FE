44483b97967c1498f677ba450dbb7dce5d10de4c
moonix0 <moongraph21@gmail.com>
2022-12-21 10:58:10 +0700

    feat/apis: Update Profile

--- src/store/features/index.js
+++ src/store/features/index.js
@@ -4,6 +4,7 @@ import session from './sessionSlice';
 import booking from './bookingSlice';
 import vaccine from './vaccineSlice';
 import dashboard from './dashboardSlice';
+import profile from './profileSlice';
 
 export const rootReducers = combineReducers({
   admin,
@@ -11,4 +12,5 @@ export const rootReducers = combineReducers({
   session,
   booking,
   vaccine,
+  profile,
 });
